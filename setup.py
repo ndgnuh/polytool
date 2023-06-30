e8dc45c537703c651dd4ddf0bd2a2de3fa32ebcf
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-06-30 21:42:46 +0700

    [build] fix wheel building, maybe

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+from os import path
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize("polytool/polygon.pyx"))
+setup(
+    include_package_data=False,
+    setup_requires=["cython"],
+    ext_modules=cythonize(["*/*.pyx"]),
+)
